a0b74196614d144460d7689cbb5a1ce7251cdcd7
Jesper Larsson <jesper@lolo.company>
2023-04-05 15:55:23 +0200

    Add greet count to root

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,24 @@ use axum::routing::get;
 #[tokio::main]
 async fn main() {
 	let db = data::Database::new().await;
+	let root_db = db.clone();
+	let path_db = db.clone();
 
 	let app = Router::new()
-		.route("/", get(|| async { "Hello, World!" }))
+		.route("/", get(|| async move {
+			let count = root_db.inc_and_get_count(&"")
+				.await;
+
+			let Ok(count) = count else {
+				return (StatusCode::INTERNAL_SERVER_ERROR, "An error occurred".to_string())
+			};
+
+			let greeting = format!("Hello, World!\nI've greeted you {count} time(s).");
+
+			(StatusCode::OK, greeting)
+		}))
 		.route("/:name", get(|Path(user_id): Path<String>| async move {
-			let count = db.inc_and_get_count(&user_id)
+			let count = path_db.inc_and_get_count(&user_id)
 				.await;
 
 			let Ok(count) = count else {
